9d15ac518bb16a4bd0a0f94115b39857e36265ee
ike.lionel <ike.lionel@LAPTOP14129>
2025-10-23 09:31:53 +0100

    Add project files.

---
+++ MathQuiz.py
@@ -0,0 +1,16 @@
+# Import Tkinter Libraries
+
+
+# Create Window with title and set size of window
+
+
+# Create Frames to organise widgets
+
+
+#Create Widgets and images to use for UI
+
+
+#Pack frames and widgets in order you want on page
+
+
+# Create window with elements 
