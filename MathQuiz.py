fc45ee3dbd9bf536f647e1f5f277905b62abfb7c
AYOUB TARHZOUTI <514470@my.cwc.ac.uk>
2025-10-24 18:10:58 +0100

    Fixed PIL & Fixed Image Labels

--- MathQuiz.py
+++ MathQuiz.py
@@ -33,21 +33,29 @@ btna.place(x=30, y=180, width=125, height=50)
 btns = tk.Button(window, text="Subtraction", command=Sub)
 btns.place(x=30, y=260, width=125, height=50)
 
-#Create Widgets and images to use for UI
-images = [
-    tk.PhotoImage(file=f"Images/{i}.png") for i in range(10)
-]
-num1 = random.choice(images)
-label = tk.Label(window, image=num1)
-num1 = image.resize((50, 50))
-label.place(x=150, y=150, width=50, height=50)
+#Generate Random First Number
+image_paths = [f"Images/{i}.png" for i in range(10)]
 
-images = [
-    tk.PhotoImage(file=f"Images/{i}.png") for i in range(10)
-]
-num2 = random.choice(images)
-label = tk.Label(window, image=num2)
-label.place(x=300, y=150, width=50, height=50)
+#First Number Image
+num1 = Image.open(random.choice(image_paths))
+num1 = num1.resize((75, 75))
+num1 = ImageTk.PhotoImage(num1)
+
+label1 = tk.Label(window, image=num1)
+label1.image = num1
+label1.place(x=175, y=150, width=75, height=75)
+
+#Generate Second Number Image
+image_paths = [f"Images/{i}.png" for i in range(10)]
+
+#Second Number Image
+num2 = Image.open(random.choice(image_paths))
+num2 = num2.resize((75, 75))
+num2 = ImageTk.PhotoImage(num2)
+
+label2 = tk.Label(window, image=num2)
+label2.image = num2
+label2.place(x=350, y=150, width=75, height=75)
 
 
 #Pack frames and widgets in order you want on page
