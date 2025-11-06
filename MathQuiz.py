08734cf67a591ba403c9fa6f525a2d5c0b465fb5
AYOUB TARHZOUTI <514470@my.cwc.ac.uk>
2025-11-06 09:03:52 +0000

    Moved the random number code

--- MathQuiz.py
+++ MathQuiz.py
@@ -11,10 +11,36 @@ window = tk.Tk()
 window.title('Simple Math Game')
 window.geometry('700x400')  
 
+def RandNum():
+    #Generate Random First Number
+    image_paths = [f"Images/{i}.png" for i in range(10)]
+
+    #First Number Image
+    num1 = Image.open(random.choice(image_paths))
+    num1 = num1.resize((75, 75))
+    num1 = ImageTk.PhotoImage(num1)
+
+    label1 = tk.Label(window, image=num1)
+    label1.image = num1
+    label1.place(x=225, y=125, width=75, height=75)
+
+    #Generate Second Number Image
+    image_paths = [f"Images/{i}.png" for i in range(10)]
+
+    #Second Number Image
+    num2 = Image.open(random.choice(image_paths))
+    num2 = num2.resize((75, 75))
+    num2 = ImageTk.PhotoImage(num2)
+
+    label2 = tk.Label(window, image=num2)
+    label2.image = num2
+    label2.place(x=400, y=125, width=75, height=75)
+
 #Check What Mode To Run
 def Start():
     match Mode:
         case "Mul":
+            RandNum()
             print("Mul")
             oper = Image.open(r"C:\Users\ayoub\source\repos\KidsMathGame\Images\multiply.png")
             oper = oper.resize((75, 75))
@@ -24,6 +50,7 @@ def Start():
             Mlabel.image = opertk
             Mlabel.place(x=315, y=125, width=75, height=75)
         case "Div":
+             RandNum()
              print("Div")
              oper = Image.open(r"C:\Users\ayoub\source\repos\KidsMathGame\Images\divide.png")
              oper = oper.resize((75, 75))
@@ -33,6 +60,7 @@ def Start():
              Dlabel.image = opertk
              Dlabel.place(x=315, y=125, width=75, height=75)
         case "Add":
+            RandNum()
             print("Add")
             oper = Image.open(r"C:\Users\ayoub\source\repos\KidsMathGame\Images\plus.png")
             oper = oper.resize((75, 75))
@@ -42,6 +70,7 @@ def Start():
             Alabel.image = opertk
             Alabel.place(x=315, y=125, width=75, height=75)
         case "Sub":
+            RandNum()
             print("Sub")
             oper = Image.open(r"C:\Users\ayoub\source\repos\KidsMathGame\Images\minus.png")
             oper = oper.resize((75, 75))
@@ -85,31 +114,6 @@ btna = tk.Button(window, text="Addition", command=Add)
 btna.place(x=30, y=180, width=125, height=50)
 btns = tk.Button(window, text="Subtraction", command=Sub)
 btns.place(x=30, y=260, width=125, height=50)
-
-
-#Generate Random First Number
-image_paths = [f"Images/{i}.png" for i in range(10)]
-
-#First Number Image
-num1 = Image.open(random.choice(image_paths))
-num1 = num1.resize((75, 75))
-num1 = ImageTk.PhotoImage(num1)
-
-label1 = tk.Label(window, image=num1)
-label1.image = num1
-label1.place(x=225, y=125, width=75, height=75)
-
-#Generate Second Number Image
-image_paths = [f"Images/{i}.png" for i in range(10)]
-
-#Second Number Image
-num2 = Image.open(random.choice(image_paths))
-num2 = num2.resize((75, 75))
-num2 = ImageTk.PhotoImage(num2)
-
-label2 = tk.Label(window, image=num2)
-label2.image = num2
-label2.place(x=400, y=125, width=75, height=75)
 
 
 #Pack frames and widgets in order you want on page
